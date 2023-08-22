de55fce6f0b267e5e45eb32b13efa39716fc0462
TatzkDepression <tatzkrebecca@gmail.com>
2023-08-22 21:55:47 +0700

    update

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -1,4 +1,5 @@
 // render dienthoai
+
 const totalItemInCart = document.querySelector("#cartCount");
 function DanhSachSanPham(products) {
   document.querySelector("#productList").innerHTML = products
@@ -65,7 +66,7 @@ function updateQuantity() {
   totalItemInCart.innerHTML = quantity;
 }
 
-//hàm cập nhật số lượng sản phẩm trong giỏ hàng và giao diện
+//hàm cập nhật số lượnggit sản phẩm trong giỏ hàng và giao diện
 function updateCartItemQuantity(productId, newQuantity) {
   const cartItem = cart.find((item) => item.id === productId);
   if (cartItem) {
@@ -76,59 +77,71 @@ function updateCartItemQuantity(productId, newQuantity) {
 ////////////////////////////////////////////////////
 // xuất giỏ hàng ra cart.html
 /////////////////////////////////
-for (let i = 0; i < cart.length; i++) {
-  axios({
-    url: `${api}/${cart[i].id}`,
+// Khởi tạo một mảng chứa các đoạn mã HTML sản phẩm
+const cartItemHTML = cart.map((cartItem) => {
+  return axios({
+    url: `${api}/${cartItem.id}`,
     method: "GET",
   })
     .then((res) => {
-      //res.data
-      let cartPro = res.data;
-      let divString = `
-      <div class="shopping-cart grid gap-4 justify-center">
-        <div class="cart-item flex border border-solid border-black m-6 p-3">
-          <img class="w-40" src=${cartPro.img} alt="" />
-          <div class="details flex flex-col ml-4">
-            <div class="title-price-x w-48 flex items-center justify-between">
-              <h4 class="title-price flex items-center gap-3 ">
-                <p>${cartPro.name}</p>
-                <p class="cart-item-price bg-black text-white rounded-md py-1 px-2">${cartPro.price.toLocaleString(
-                  "en-US",
-                  {
-                    style: "currency",
-                    currency: "USD",
-                  }
-                )}</p>
-              </h4>
-              <i onclick="removeItem(${
-                cartPro.id
-              })" class="bi bi-x-lg font-bold text-red-600 cursor-pointer"></i>
+      const cartPro = res.data;
+      return `
+        <div class="shopping-cart grid gap-4 justify-center">
+          <div class="cart-item flex border border-solid border-black m-6 p-3">
+            <img class="w-40" src=${cartPro.img} alt="" />
+            <div class="details flex flex-col ml-4">
+              <div class="title-price-x w-48 flex items-center justify-between">
+                <h4 class="title-price flex items-center gap-3 ">
+                  <p>${cartPro.name}</p>
+                  <p class="cart-item-price bg-black text-white rounded-md py-1 px-2">${cartPro.price.toLocaleString(
+                    "en-US",
+                    {
+                      style: "currency",
+                      currency: "USD",
+                    }
+                  )}</p>
+                </h4>
+                <i onclick="removeItem(${
+                  cartPro.id
+                })" class="bi bi-x-lg font-bold text-red-600 cursor-pointer"></i>
+              </div>
+              <div class="buttons flex flex-row gap-2 text-base items-center">
+                <i onclick="giam(${
+                  cartPro.id
+                })" class="bi bi-dash-lg text-red-600 cursor-pointer"></i>
+                <div id="${cartPro.id}" class="quantity">${
+        cartItem.quantity
+      }</div>
+                <i onclick="tang(${
+                  cartPro.id
+                })" class="bi bi-plus-lg text-green-600 cursor-pointer"></i>
+              </div>
+              <h3 class="text-lg mt-2">${cartPro.price.toLocaleString("en-US", {
+                style: "currency",
+                currency: "USD",
+              })}</h3>
             </div>
-            <div class="buttons flex flex-row gap-2 text-base items-center">
-              <i onclick="giam(${
-                cartPro.id
-              })" class="bi bi-dash-lg text-red-600 cursor-pointer"></i>
-              <div id="${cartPro.id}" class="quantity">${cart[i].quantity}</div>
-              <i onclick="tang(${
-                cartPro.id
-              })" class="bi bi-plus-lg text-green-600 cursor-pointer"></i>
-            </div>
-            <h3 class="text-lg mt-2">${cartPro.price.toLocaleString("en-US", {
-              style: "currency",
-              currency: "USD",
-            })}</h3>
           </div>
         </div>
-      </div>
-    `;
-
-      contentHTML += divString;
-      label.innerHTML = contentHTML;
+      `;
     })
     .catch((err) => {
       console.log(err);
     });
-}
+});
+
+Promise.all(cartItemHTML)
+  .then((htmlArray) => {
+    // Nối tất cả các đoạn mã HTML thành một chuỗi HTML duy nhất
+    const contentHTML = htmlArray.join("");
+
+    // Thêm chuỗi HTML vào label
+    label.innerHTML = contentHTML;
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 // ///////////////
 // xóa giỏ hàng
 function xoaGioHang() {
