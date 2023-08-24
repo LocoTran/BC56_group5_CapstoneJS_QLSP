4741a884b6e692a8efde16a66570491712af0e92
TatzkDepression <tatzkrebecca@gmail.com>
2023-08-24 19:57:10 +0700

    update

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,5 @@
+const api = `https://64cde6de0c01d81da3ee5a2f.mockapi.io/phones`;
+
 function fetchProductList() {
   axios({
     url: api,
--- models/Carts.js
+++
@@ -1,4 +0,0 @@
-const api = `https://64cde6de0c01d81da3ee5a2f.mockapi.io/phones`;
-let label = document.querySelector("#label");
-let gioHang = JSON.parse(localStorage.getItem("CART")) || [];
-let contentHTML = "";
