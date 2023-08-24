bdefb38f48ef28cce0acac5404b7dc66ddb8a8c6
TatzkDepression <tatzkrebecca@gmail.com>
2023-08-24 19:49:53 +0700

    fix vài lỗi

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -1,56 +1,95 @@
 // render dienthoai
 const totalItemInCart = document.querySelector("#cartCount");
+
 function DanhSachSanPham(products) {
-  document.querySelector("#productList").innerHTML = products
-    .map((product) => {
-      return `
-        <div class="item mr-4 p-2 mt-4">
-          <img src="${product.img}" alt="">
-            <div class="content">
-                <div class="detail">
-                  <h4 class="text-xl font-semibold py-2">${product.name}</h4>
-                  <p><span>${product.screen}</span></p>
-                  <p>Camera sau: <span>${product.backCamera}</span></p>
-                  <p>Camera trước: <span>${product.frontCamera}</span></p>
-                  <p>${product.desc}</p>
-                </div>
-            </div>
-            <div class="price">
-              <h4 class="text-lg font-semibold py-1">${product.price}$</h4>
-            </div>
-            <div>
-              <i class="fa fa-star text-yellow-400" aria-hidden="true"></i>
-                      <i class="fa fa-star text-yellow-400" aria-hidden="true"></i>
-                      <i class="fa fa-star text-yellow-400" aria-hidden="true"></i>
-                      <i class="fa fa-star text-yellow-400" aria-hidden="true"></i>
-                      <i class="fa fa-star" aria-hidden="true"></i>
-            </div>
-            <button onclick="addToCart(${product.id})" class="btnAdd"><i class="fa-solid fa-cart-shopping"></i>Add to cart</button>
-        </div>
-        `;
-    })
-    .join(" ");
+  const productList = document.querySelector("#productList");
+
+  if (productList) {
+    productList.innerHTML = products
+      .map((product) => {
+        return `
+          <div class="item mr-4 p-2 mt-4">
+            <img src="${product.img}" alt="">
+              <div class="content">
+                  <div class="detail">
+                    <h4 class="text-xl font-semibold py-2">${product.name}</h4>
+                    <p><span>${product.screen}</span></p>
+                    <p>Camera sau: <span>${product.backCamera}</span></p>
+                    <p>Camera trước: <span>${product.frontCamera}</span></p>
+                    <p>${product.desc}</p>
+                  </div>
+              </div>
+              <div class="price">
+                <h4 class="text-lg font-semibold py-1">${product.price}$</h4>
+              </div>
+              <div>
+                <i class="fa fa-star text-yellow-400" aria-hidden="true"></i>
+                        <i class="fa fa-star text-yellow-400" aria-hidden="true"></i>
+                        <i class="fa fa-star text-yellow-400" aria-hidden="true"></i>
+                        <i class="fa fa-star text-yellow-400" aria-hidden="true"></i>
+                        <i class="fa fa-star" aria-hidden="true"></i>
+              </div>
+              <button onclick="addToCart(${product.id})" class="btnAdd"><i class="fa-solid fa-cart-shopping"></i>Add to cart</button>
+          </div>
+          `;
+      })
+      .join(" ");
+  } else {
+    // console.error("Không tìm thấy phần tử có id là 'productList'");
+  }
 }
-///////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////
+let productInfoCache = {}; // Đối tượng để lưu trữ thông tin sản phẩm đã lấy từ API
+
+// function để lấy thông tin sản phẩm từ API hoặc cache
+async function getProductInfoById(id) {
+  if (productInfoCache[id]) {
+    // Nếu đã lưu trữ thông tin sản phẩm, trả về nó từ cache
+    return productInfoCache[id];
+  } else {
+    // Nếu chưa có thông tin sản phẩm, gửi yêu cầu API và lưu vào cache
+    try {
+      const response = await axios.get(`${api}/${id}`);
+      const productInfo = response.data;
+      productInfoCache[id] = productInfo; // Lưu thông tin vào cache
+      return productInfo;
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  }
+}
+//////////////////////////////////////////////////////
 // giỏ hàng
-// cart array
 let cart = JSON.parse(localStorage.getItem("CART")) || [];
 updateQuantity();
 
 // add to cart
 window.addToCart = addToCart;
-function addToCart(id) {
-  const existingItem = cart.find((item) => item.id === id); // quan . item
-  if (existingItem) {
-    existingItem.quantity += 1;
-  } else {
-    cart.push({ id: id, quantity: 1 });
+async function addToCart(id) {
+  if (isProductInCart(id)) {
+    // Sản phẩm đã tồn tại trong giỏ hàng, không thêm mới
+    alert(
+      "Sản phẩm đã có trong giỏ hàng. Hãy vào giỏ hàng để tăng giảm số lượng"
+    );
+    return;
   }
-  updateQuantity();
-  calculateTotalBill();
-  updateCart(); // Lưu lại giỏ hàng vào local storage
-}
 
+  const productInfo = await getProductInfoById(id);
+  if (productInfo) {
+    cart.push({
+      id: id,
+      quantity: 1,
+      price: productInfo.price,
+    });
+  }
+
+  updateQuantity();
+  updateCart();
+  calculateTotalBill();
+}
+//////////////////////////////////////////////////////
 // update cart
 function updateCart() {
   localStorage.setItem("CART", JSON.stringify(cart));
@@ -61,84 +100,87 @@ function updateQuantity() {
   const quantity = cart.reduce((total, item) => total + item.quantity, 0);
   totalItemInCart.innerHTML = quantity;
 }
-
-//hàm cập nhật số lượnggit sản phẩm trong giỏ hàng và giao diện
+//////////////////////////////////////////////////////
+//function cập nhật số lượng sản phẩm trong giỏ hàng và giao diện
 function updateCartItemQuantity(productId, newQuantity) {
   const cartItem = cart.find((item) => item.id === productId);
   if (cartItem) {
     cartItem.quantity = newQuantity;
-    updateCart();
+    updateCart(); // Lưu lại giỏ hàng vào local storage
+    updateQuantity(); // Cập nhật số lượng sản phẩm trong giỏ hàng và giao diện
+    calculateTotalBill(); // Cập nhật tổng tiền sau khi cập nhật số lượng
   }
 }
-////////////////////////////////////////////////////
+//////////////////////////////////////////////////////
 // xuất giỏ hàng ra cart.html
-/////////////////////////////////
 // Khởi tạo một mảng chứa các đoạn mã HTML sản phẩm
-const cartItemHTML = cart.map((cartItem) => {
-  return axios({
-    url: `${api}/${cartItem.id}`,
-    method: "GET",
-  })
-    .then((res) => {
-      const cartPro = res.data;
-      return `
-        <div class="shopping-cart grid gap-4 justify-center">
-          <div class="cart-item flex border border-solid border-black m-6 p-3">
-            <img class="w-40" src=${cartPro.img} alt="" />
-            <div class="details flex flex-col ml-4">
-              <div class="title-price-x w-48 flex items-center justify-between">
-                <h4 class="title-price flex items-center gap-3 ">
-                  <p>${cartPro.name}</p>
-                  <p class="cart-item-price bg-black text-white rounded-md py-1 px-2">${cartPro.price.toLocaleString(
+async function renderCart() {
+  try {
+    const contentHTML = await Promise.all(
+      cart.map(async (cartItem) => {
+        try {
+          const response = await axios.get(`${api}/${cartItem.id}`);
+          const cartPro = response.data;
+          return `
+            <div class="shopping-cart grid gap-4 justify-center">
+              <div class="cart-item flex border border-solid border-black m-6 p-3">
+                <img class="w-40" src=${cartPro.img} alt="" />
+                <div class="details flex flex-col ml-4">
+                  <div class="title-price-x w-48 flex items-center justify-between">
+                    <h4 class="title-price flex items-center gap-3 ">
+                      <p>${cartPro.name}</p>
+                    </h4>
+                    <i onclick="removeItem(${
+                      cartPro.id
+                    })" class="bi bi-x-lg font-bold text-red-600 cursor-pointer"></i>
+                  </div>
+                  <div class="buttons flex flex-row gap-2 text-base items-center">
+                    <i onclick="giam(${
+                      cartPro.id
+                    })" class="bi bi-dash-lg text-red-600 cursor-pointer"></i>
+                    <div id="${cartPro.id}" class="quantity">${
+            cartItem.quantity
+          }</div>
+                    <i onclick="tang(${
+                      cartPro.id
+                    })" class="bi bi-plus-lg text-green-600 cursor-pointer"></i>
+                  </div>
+                  <h3 class="text-lg mt-2" id="giaTien1SanPham">${cartPro.price.toLocaleString(
                     "en-US",
                     {
                       style: "currency",
                       currency: "USD",
                     }
-                  )}</p>
-                </h4>
-                <i onclick="removeItem(${
-                  cartPro.id
-                })" class="bi bi-x-lg font-bold text-red-600 cursor-pointer"></i>
+                  )}</h3>
+                </div>
               </div>
-              <div class="buttons flex flex-row gap-2 text-base items-center">
-                <i onclick="giam(${
-                  cartPro.id
-                })" class="bi bi-dash-lg text-red-600 cursor-pointer"></i>
-                <div id="${cartPro.id}" class="quantity">${
-        cartItem.quantity
-      }</div>
-                <i onclick="tang(${
-                  cartPro.id
-                })" class="bi bi-plus-lg text-green-600 cursor-pointer"></i>
-              </div>
-              <h3 class="text-lg mt-2">${cartPro.price.toLocaleString("en-US", {
-                style: "currency",
-                currency: "USD",
-              })}</h3>
             </div>
-          </div>
-        </div>
-      `;
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+          `;
+        } catch (err) {
+          console.log(err);
+          return ""; // Trả về chuỗi rỗng nếu có lỗi
+        }
+      })
+    );
 
-Promise.all(cartItemHTML)
-  .then((htmlArray) => {
-    // Nối tất cả các đoạn mã HTML thành một chuỗi HTML duy nhất
-    const contentHTML = htmlArray.join("");
+    // Kiểm tra xem phần tử có id là "label" có tồn tại không
+    const label = document.getElementById("label");
 
-    // Thêm chuỗi HTML vào label
-    label.innerHTML = contentHTML;
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+    if (label) {
+      // Nếu tồn tại, thêm chuỗi HTML vào label
+      label.innerHTML = contentHTML.join("");
+    } else {
+      // console.error("Không tìm thấy phần tử có id là 'label'");
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
 
-// ///////////////
+// Gọi function để render giỏ hàng
+renderCart();
+
+//////////////////////////////////////////////////////
 // xóa giỏ hàng
 function xoaGioHang() {
   // Xóa toàn bộ nội dung của giỏ hàng
@@ -146,14 +188,20 @@ function xoaGioHang() {
   updateCart();
   refreshPage();
 }
-// Hàm để loại bỏ một sản phẩm khỏi giỏ hàng
+//////////////////////////////////////////////////////
+// function để loại bỏ một sản phẩm khỏi giỏ hàng
 function removeItem(productId) {
   cart = cart.filter((item) => item.id !== productId);
   updateCart();
   refreshPage();
 }
-
-//hàm tăng số lượng
+//////////////////////////////////////////////////////
+// Kiểm tra sản phẩm đã có trong giỏ hàng chưa
+function isProductInCart(productId) {
+  return cart.some((item) => item.id === productId);
+}
+//////////////////////////////////////////////////////
+//function tăng số lượng
 function tang(productId) {
   const quantityElement = document.getElementById(productId);
   const currentQuantity = parseInt(quantityElement.innerText);
@@ -162,8 +210,8 @@ function tang(productId) {
   updateQuantity(); // Cập nhật số lượng tổng hiển thị
   calculateTotalBill();
 }
-
-//hàm giảm số lượng
+//////////////////////////////////////////////////////
+//function giảm số lượng
 function giam(productId) {
   const quantityElement = document.getElementById(productId);
   const currentQuantity = parseInt(quantityElement.innerText);
@@ -175,46 +223,47 @@ function giam(productId) {
     calculateTotalBill();
   }
 }
-
-/////////////////////////////////////////////////////////
-// Gọi updateQuantity khi trang đã tải xong
-window.onload = function () {
-  updateQuantity();
-};
-// Làm mới trang để thể hiện giỏ hàng đã được cập nhật
-
-// /////////////////////
-// Hàm để tính tổng tiền của các sản phẩm trong giỏ hàng
-function calculateTotalBill() {
-  let totalBill = 0;
-  for (const item of cart) {
-    axios({
-      url: `${api}/${item.id}`,
-      method: "GET",
-    })
-      .then((res) => {
-        let cartPro = res.data;
-        totalBill += cartPro.price * item.quantity;
-        updateTotalBill(totalBill);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }
-}
-
-// Hàm để cập nhật tổng tiền trong giao diện
+//////////////////////////////////////////////////////
+// function để cập nhật tổng tiền trong giao diện
 function updateTotalBill(totalBill) {
   const totalBillElement = document.getElementById("totalBill");
-  totalBillElement.textContent = `Tổng Tiền: ${totalBill.toLocaleString(
-    "en-US",
-    { style: "currency", currency: "USD" }
-  )}`;
+
+  if (totalBillElement) {
+    totalBillElement.textContent = `Tổng Tiền: ${totalBill.toLocaleString(
+      "en-US",
+      { style: "currency", currency: "USD" }
+    )}`;
+  } else {
+    // console.error("Không tìm thấy phần tử có id là 'totalBill'");
+  }
 }
+//////////////////////////////////////////////////////
+// function để tính tổng tiền của các sản phẩm trong giỏ hàng
+async function calculateTotalBill() {
+  const promises = cart.map((item) => getProductInfoById(item.id));
+
+  Promise.all(promises)
+    .then((productInfos) => {
+      let totalBill = 0;
+
+      productInfos.forEach((productInfo, index) => {
+        if (productInfo) {
+          totalBill += productInfo.price * cart[index].quantity;
+        }
+      });
+
+      updateTotalBill(totalBill);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+//////////////////////////////////////////////////////
 function refreshPage() {
   location.reload();
 }
-// Gọi hàm tính tổng tiền khi trang đã tải xong
+// Gọi function tính tổng tiền và updateQuantity khi trang đã tải xong
 window.onload = function () {
   calculateTotalBill();
+  updateQuantity();
 };
